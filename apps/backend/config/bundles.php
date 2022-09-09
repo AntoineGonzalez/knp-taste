199019df546daff1f0f4cf67bac126604e2b5d60
AntoineGonzalez <45924026+AntoineGonzalez@users.noreply.github.com>
2022-09-09 09:05:16 +0200

    refactoring: remove useless dependencies, remove user admin flag & remove useless getters/setters.

--- apps/backend/config/bundles.php
+++ apps/backend/config/bundles.php
@@ -8,6 +8,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true]
 ];
