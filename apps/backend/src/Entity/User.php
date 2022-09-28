3e6b4067de001e1bffc5e864c0d9e0b8ff6a628f
AntoineGonzalez <45924026+AntoineGonzalez@users.noreply.github.com>
2022-09-28 09:04:04 +0200

    feature: change user getUsername

--- apps/backend/src/Entity/User.php
+++ apps/backend/src/Entity/User.php
@@ -31,7 +31,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getUsername(): string
     {
-        return $this->username;
+        return $this->email;
     }
 
     /**
